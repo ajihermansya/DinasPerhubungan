387dfe42f69de0a57f9675bc864e0181ad07128d
ajihermansya <ajiheransya14@gmail.com>
2023-12-30 09:47:37 +0700

    register ui/ux && animation login-done

--- app/src/main/java/com/dinas/perhubungan/ui/loginregis/LoginActivity.kt
+++ app/src/main/java/com/dinas/perhubungan/ui/loginregis/LoginActivity.kt
@@ -1,12 +1,37 @@
 package com.dinas.perhubungan.ui.loginregis
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.dinas.perhubungan.R
+import com.dinas.perhubungan.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        binding.tvDaftar.setOnClickListener {  startActivity(Intent(this, RegisterActivity::class.java))  }
+
+        Animation()
     }
+
+    private fun Animation() {
+        ObjectAnimator.ofFloat(binding.imageView3, View.TRANSLATION_Y, -20f, 20f).apply {
+            duration = 4000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
+
+    }
+
+
 }
--- app/src/main/java/com/dinas/perhubungan/ui/loginregis/RegisterActivity.kt
+++ app/src/main/java/com/dinas/perhubungan/ui/loginregis/RegisterActivity.kt
@@ -3,10 +3,15 @@ package com.dinas.perhubungan.ui.loginregis
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dinas.perhubungan.R
+import com.dinas.perhubungan.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
     }
 }
