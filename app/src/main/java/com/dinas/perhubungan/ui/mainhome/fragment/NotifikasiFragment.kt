c2a97eaf596c7c3790d79d0872bd3280feda8657
ajihermansya <ajiheransya14@gmail.com>
2024-01-05 19:42:19 +0700

    notifikasi-done

--- app/src/main/java/com/dinas/perhubungan/ui/mainhome/fragment/NotifikasiFragment.kt
+++ app/src/main/java/com/dinas/perhubungan/ui/mainhome/fragment/NotifikasiFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import com.dinas.perhubungan.R
 import com.dinas.perhubungan.databinding.FragmentHomeBinding
 import com.dinas.perhubungan.databinding.FragmentNotifikasiBinding
@@ -19,6 +20,8 @@ class NotifikasiFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentNotifikasiBinding.inflate(inflater, container, false)
 
+        val notif = binding.root.findViewById<LinearLayout>(R.id.notif_switch)
+        val tanggal = binding.root.findViewById<LinearLayout>(R.id.tanggal_notif )
 
         return binding.root
     }
--- app/src/main/java/com/dinas/perhubungan/ui/mainhome/person/NotifikasiActivity.kt
+++ app/src/main/java/com/dinas/perhubungan/ui/mainhome/person/NotifikasiActivity.kt
@@ -3,10 +3,20 @@ package com.dinas.perhubungan.ui.mainhome.person
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dinas.perhubungan.R
+import com.dinas.perhubungan.databinding.ActivityNotifikasiBinding
+import com.dinas.perhubungan.databinding.ActivityRegisterBinding
 
 class NotifikasiActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNotifikasiBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notifikasi)
+        binding = ActivityNotifikasiBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        binding.iconBack.setOnClickListener {
+            finish()
+        }
     }
 }
